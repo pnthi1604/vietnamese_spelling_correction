9db63a41e4fee5528d45dcea882636d34bf4bb0f
phamngocthi <pnthi.it.cp@gmail.com>
2024-04-07 11:41:58 +0700

    fix pred_ids detach clone

--- val.py
+++ val.py
@@ -39,15 +39,19 @@ def validation(model, config, tokenizer_src, tokenizer_tgt, validation_dataloade
                                 src=src,
                                 src_mask=src_mask)
         
-        print(f"{pred_ids = }")
-        print(f"{label_ids = }")
+        # print(f"{pred_ids = }")
+        # print(f"{label_ids = }")
         pred_text = tokenizer_tgt.decode(pred_ids.detach().cpu().numpy())
 
         padding = pad_sequence([label_ids, pred_ids], padding_value=pad_index, batch_first=True)
         label_ids = padding[0]
         pred_ids = padding[1]
-        print(f"{label_ids = }")
-        print(f"{pred_ids = }")
+        # print(f"{label_ids = }")
+        # print(f"{pred_ids = }")
+
+        recall = calc_recall(preds=pred_ids, target=label_ids, tgt_vocab_size=tgt_vocab_size, pad_index=pad_index, device=device)
+        precision = calc_precision(preds=pred_ids, target=label_ids, tgt_vocab_size=tgt_vocab_size, pad_index=pad_index, device=device)
+        f_05 = calc_f_beta(preds=pred_ids, target=label_ids, beta=0.5, tgt_vocab_size=tgt_vocab_size, pad_index=pad_index, device=device)
         
         labels.append(label_ids)
         preds.append(pred_ids)
